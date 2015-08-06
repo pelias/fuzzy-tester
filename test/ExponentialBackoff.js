e508620cff42ebb7ebd7ca92edff06f35d11ca6d
Peter Johnson <insomnia@rcpt.at>
2015-08-06 10:28:46 +0200

    fix indentation

--- test/ExponentialBackoff.js
+++ test/ExponentialBackoff.js
@@ -4,30 +4,30 @@ var tape = require( 'tape' );
 var ExponentialBackoff = require( '../lib/ExponentialBackoff' );
 
 tape( 'ExponentialBackoff', function(test) {
-    test.test( 'calling increaseBackoff() makes backoff larger', function(t) {
-        var eb = new ExponentialBackoff();
-        var startBackoff = eb.getBackoff();
-        eb.increaseBackoff();
-        t.ok(startBackoff < eb.getBackoff(), 'backoff was higher');
-        t.end();
-    });
+  test.test( 'calling increaseBackoff() makes backoff larger', function(t) {
+    var eb = new ExponentialBackoff();
+    var startBackoff = eb.getBackoff();
+    eb.increaseBackoff();
+    t.ok(startBackoff < eb.getBackoff(), 'backoff was higher');
+    t.end();
+  });
 
-    test.test( 'calling decreaseBackoff() lowers backoff', function(t) {
-        var eb = new ExponentialBackoff();
-        eb.increaseBackoff();
-        var startBackoff = eb.getBackoff();
-        eb.decreaseBackoff();
-        t.ok(startBackoff > eb.getBackoff(), 'backoff was lower');
-        t.end();
-    });
+  test.test( 'calling decreaseBackoff() lowers backoff', function(t) {
+    var eb = new ExponentialBackoff();
+    eb.increaseBackoff();
+    var startBackoff = eb.getBackoff();
+    eb.decreaseBackoff();
+    t.ok(startBackoff > eb.getBackoff(), 'backoff was lower');
+    t.end();
+  });
 
-    test.test( 'calling decreaseBackoff() will not lower backoff below minimum', function(t) {
-        var eb = new ExponentialBackoff();
-        var startBackoff = eb.getBackoff();
-        eb.decreaseBackoff();
-        t.ok(startBackoff === eb.getBackoff(), 'backoff was at minimum');
-        t.end();
-    });
+  test.test( 'calling decreaseBackoff() will not lower backoff below minimum', function(t) {
+    var eb = new ExponentialBackoff();
+    var startBackoff = eb.getBackoff();
+    eb.decreaseBackoff();
+    t.ok(startBackoff === eb.getBackoff(), 'backoff was at minimum');
+    t.end();
+  });
 
-    test.end();
+  test.end();
 });
