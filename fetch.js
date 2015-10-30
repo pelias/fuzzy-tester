35bfd28998efb20764049563ab29affd75e5da7b
Peter Johnson <insomnia@rcpt.at>
2015-10-30 14:28:09 +0100

    commit files from local hdd

--- fetch.js
+++ fetch.js
@@ -4,7 +4,6 @@ var TestCase = require('./lib/TestCase'),
     LocalDataStore = require('./lib/LocalDataStore');
 
 var report = require('./lib/reporter/status');
-
 var client = new HttpClient();
 
 function load( filename ){
@@ -17,27 +16,7 @@ function load( filename ){
 }
 
 var files = process.argv.slice(2);
-if( !files.length ){
-  files = [
-    '/var/www/pelias/acceptance-tests/test_cases/address_parsing.json',
-    '/var/www/pelias/acceptance-tests/test_cases/address_type.json',
-    '/var/www/pelias/acceptance-tests/test_cases/admin_lookup.json',
-    '/var/www/pelias/acceptance-tests/test_cases/categories.json',
-    '/var/www/pelias/acceptance-tests/test_cases/exact_matches.json',
-    '/var/www/pelias/acceptance-tests/test_cases/landmarks.json',
-    '/var/www/pelias/acceptance-tests/test_cases/params_details.json',
-    '/var/www/pelias/acceptance-tests/test_cases/quattroshapes_popularity.json',
-    '/var/www/pelias/acceptance-tests/test_cases/reverse_categories.json',
-    '/var/www/pelias/acceptance-tests/test_cases/search_coarse.json',
-    '/var/www/pelias/acceptance-tests/test_cases/search.json',
-    '/var/www/pelias/acceptance-tests/test_cases/suggest_coarse.json',
-    '/var/www/pelias/acceptance-tests/test_cases/suggest.json',
-    '/var/www/pelias/acceptance-tests/test_cases/suggest_nearby.json'
-  ].reverse();
-}
-
 files.forEach( load );
-// load( '/var/www/pelias/acceptance-tests/test_cases/address_parsing.json' );
 
 var store = new LocalDataStore({ path: './history.json' });
 client.on( 'transaction', store.put.bind( store ) );
