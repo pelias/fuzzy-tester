d3ef107e144ab1e4d3c4b6127c3f9dc109117952
Vesa Meskanen <vesa.meskanen@logica.com>
2016-02-08 09:23:21 +0200

    Remove WGS84 definition for proj4 (known already)

--- scripts/importHSLPoi.js
+++ scripts/importHSLPoi.js
@@ -11,10 +11,6 @@ var read_stream = fs.createReadStream(filename);
 var proj4 = require('proj4');
 proj4.defs([
   [
-    'EPSG:4326',
-    '+title=WGS 84 (long/lat) +proj=longlat +ellps=WGS84 +datum=WGS84 +units=degrees'
-  ],
-  [
     'EPSG:2392', //# KKJ / Finland zone 2
     '+proj=tmerc +lat_0=0 +lon_0=24 +k=1 +x_0=2500000 +y_0=0 +ellps=intl ' +
     '+towgs84=-96.0617,-82.4278,-121.7535,4.80107,0.34543,-1.37646,1.4964 +units=m +no_defs'
@@ -40,10 +36,7 @@ var testCaseStream = through({objectMode: true}, function(record, encoding, call
     status: 'pass',
     user: 'vesameskanen',
     in: {
-    text: record.name + ', ' + record.locality
-//    focus not needed - names are surely unique within locality
-//    focus_point_lat: 60.20,
-//    focus_point_lon: 24.93
+      text: record.name + ', ' + record.locality
     },
     expected: {
       properties: [
@@ -54,7 +47,7 @@ var testCaseStream = through({objectMode: true}, function(record, encoding, call
         }
       ],
       coordinates: [
-        proj4('EPSG:2392','EPSG:4326',[record.xpos, record.ypos])
+        proj4('EPSG:2392','WGS84',[record.xpos, record.ypos])
       ]
     }
   };
