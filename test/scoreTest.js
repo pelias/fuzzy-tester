ae68f691d8c1d102ec0a393594dfbf030d6efb66
Julian Simioni <julian@mapzen.com>
2016-04-29 15:45:19 -0400

    Show which unexpected value was found

--- test/scoreTest.js
+++ test/scoreTest.js
@@ -30,4 +30,33 @@ tape( 'scoreUnexpected basics', function ( test ){
     t.equal(result.diff, '', 'diff is empty');
     t.end();
   });
+
+  test.test('0 score returned when unexpected property found', function(t) {
+    var unexpected = {
+      properties: [
+        {
+          name: 'unexpectedName'
+        }
+      ]
+    };
+
+    var results = [
+      {
+        properties: {
+          name: 'a great name'
+        }
+      },
+      {
+        properties: {
+          name: 'unexpectedName'
+        }
+      }
+    ];
+    var result = scoreTest.scoreUnexpected(unexpected, results);
+    t.equal(result.score, 0, 'score is 0');
+    t.equal(result.max_score, 1, 'max score is 1');
+    t.deepEquals(result.diff,[ 'unexpected result found from {"name":"unexpectedName"}' ],
+                 'the diff says which unexpected result was found');
+    t.end();
+  });
 });
