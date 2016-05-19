12df0f024e5e6fd3148d5363ca0e76b8a81d5241
Julian Simioni <julian@simioni.org>
2016-05-19 17:48:38 -0400

    Extremely initial email fixing

--- output_generators/email.js
+++ output_generators/email.js
@@ -19,6 +19,14 @@ catch ( ex ) {
   process.exit( 1 );
 }
 
+// replacer for stringifying testCase to avoid circular structure
+function replace(key, value) {
+  if (key === 'results' || key === 'result') {
+    return undefined;
+  }
+  return value;
+}
+
 (function checkEmailConfig() {
   ['recipients'].forEach( function ( prop ) {
     if( !emailConfig.hasOwnProperty( prop )) {
@@ -33,50 +41,59 @@ catch ( ex ) {
   });
 })();
 
-function formatTestCase( res ){
-  var id = res.testCase.id;
-  var input = JSON.stringify( res.testCase.in, undefined, 4 );
-  var status = (res.progress === undefined) ? '' :
-    util.format( '<span class="status">%s</span> ', res.progress );
+function formatTestCase( testCase ){
+  var id = testCase.id;
+  var input = JSON.stringify( testCase.in, undefined, 4 );
+  var result = testCase.results[testCase.full_url];
+  var status = (result.progress === undefined) ? '' :
+    util.format( '<span class="status">%s</span> ', result.progress );
 
   var out;
-  switch( res.result ){
+  switch( result.result ){
     case 'pass':
       out = new handlebars.SafeString( util.format( '✔ %s[%s] "%s"', status, id, input ) );
       break;
 
     case 'fail':
-      out = new handlebars.SafeString( util.format( '✘ %s[%s] "%s": %s', status, id, input, res.msg ) );
+      out = new handlebars.SafeString( util.format( '✘ %s[%s] "%s": %s', status, id, input, result.msg ) );
       break;
 
     case 'placeholder':
-      return util.format( '! [%s] "%s": %s', id, input, res.msg );
+      return util.format( '! [%s] "%s": %s', id, input, result.msg );
 
     default:
-      console.error( util.format( 'Result type `%s` not recognized.', res.result ) );
+      console.error( util.format( 'Result type `%s` not recognized.', result.result ) );
       process.exit( 1 );
   }
 
   return out;
 }
 
-function emailResults( suiteResults  ){
+function emailResults( suiteResults , config, testSuites ){
   handlebars.registerHelper( 'json', JSON.stringify );
   handlebars.registerHelper( 'testCase', formatTestCase );
 
+  var templateParams = { suiteResults: suiteResults, config: config, testSuites: testSuites };
+
+  testSuites.forEach(function(suite) {
+    suite.tests.forEach(function(testCase) {
+      testCase.result = testCase.results[testCase.full_url];
+    });
+  });
+
   var templatePath = path.join( __dirname, 'email_static/email.html' );
   var emailTemplate = fs.readFileSync( templatePath ).toString();
-  var emailHtml = juice( handlebars.compile( emailTemplate )( suiteResults ) );
+  var emailHtml = juice( handlebars.compile( emailTemplate )( templateParams ) );
   var transporter = nodemailer.createTransport( nodemailerSesTransport( emailConfig.ses ) );
 
   var emailOpts = {
-    from: emailConfig.from || '"pelias-acceptance-tests" <noreply@pelias-acceptance-tests>',
+    from: emailConfig.from || '"pelias-acceptance-tests" <noreply@pelias.mapzen.com>',
     to: emailConfig.recipients.join( ', ' ),
     subject: 'pelias acceptance-tests results ' + new Date().toString(),
     html: emailHtml,
     attachments: [{
       filename: 'results.json',
-      content: JSON.stringify( suiteResults, undefined, 4 )
+      content: JSON.stringify( suiteResults, replace, 4 )
     }]
   };
 
