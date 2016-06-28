d31fb803c3b147d9718b733a4bb90b076ff49998
Alec Vulfson <alec@mapzen.com>
2016-06-28 17:02:28 -0400

    Add percentage for display function
    This function calculates the percentage to 2 decimal places.

--- test/test.js
+++ test/test.js
@@ -9,6 +9,7 @@ require('./ExponentialBackoff');
 require('./email/format_test_case');
 require('./eval_test');
 require('./equal_properties');
+require('./percentageForDisplay');
 require('./sanitiseTestCase');
 require('./scoreProperties');
 require('./scoreTest');
