e55ab2436eceec708055f7dc85edd2417e324ab8
Julian Simioni <julian@mapzen.com>
2016-11-18 14:13:38 -0500

    fix: fix json output failures
    The json output had not been updated in a long time, and did not reflect
    changes that had been made to the data being passed around.
    
    Fixes https://github.com/pelias/fuzzy-tester/issues/63
    Supersedes https://github.com/pelias/fuzzy-tester/pull/71

--- output_generators/json.js
+++ output_generators/json.js
@@ -9,34 +9,43 @@ var fs = require('fs-extra');
 var terminal = require('./terminal');
 var sanitize_filename = require('sanitize-filename');
 
+// replacer for stringifying testCase to avoid circular structure
+function replace(key, value) {
+  if (key === 'results' || key === 'result') {
+    return undefined;
+  }
+  return value;
+}
+
 /**
  * Format and print a test result to json file.
  */
-function saveFailTestResult( result ) {
-  if( result.result === 'fail' && result.testCase.status === 'pass' ) {
+function saveFailTestResult( testCase ) {
+  if( testCase.result.result === 'fail' && testCase.status === 'pass' ) {
     fs.ensureDirSync('./failures');
     var recordFailFile = './failures/' + sanitize_filename(
-        util.format('%s_%s.json', result.testCase.id, result.testCase.in.input));
+        util.format('%s_%s.json', testCase.id, testCase.in.text));
     var recordFail = {
-      test_case: result.testCase,
-      response: result.response.body.features
+      test_case: testCase,
+      response: testCase.result.response.body.features
     };
-    fs.writeFileSync(recordFailFile, JSON.stringify(recordFail, null, 2));
+    fs.writeFileSync(recordFailFile, JSON.stringify(recordFail, replace, 2));
   }
 }
 
 /**
  * Format and print all of the results from any number of test-suites.
  */
-function prettyPrintSuiteResults( suiteResults ) {
+function prettyPrintSuiteResults( suiteResults, config, testSuites ) {
 
-  suiteResults.results.forEach( function ( suiteResult ) {
-    suiteResult.results.forEach( function (testResult) {
-      saveFailTestResult( testResult );
+  testSuites.forEach(function(suite) {
+    suite.tests.forEach(function(testCase) {
+      testCase.result = testCase.results[testCase.full_url];
+      saveFailTestResult( testCase );
     });
   });
 
-  return terminal( suiteResults );
+  return terminal( suiteResults, config, testSuites );
 }
 
 module.exports = prettyPrintSuiteResults;
