dd9437ed9fa8adc7302a34e3183fe6ba2d6039bb
Julian Simioni <julian@simioni.org>
2020-05-25 11:38:54 -0700

    feat(api keys): Support new `credentials` config property
    This adds support for a `credentials` pelias.json config property, to
    replace the outdated `mapzen` section where config properties used to be
    required to live.
    
    The new location is preferred, but API keys can still be loaded from the
    old location. In that case, a warning will be emitted.
    
    Closes https://github.com/pelias/acceptance-tests/issues/465

--- test/apiKey.js
+++ test/apiKey.js
@@ -24,7 +24,7 @@ tape( 'api_key not found in config', function ( test ){
   test.end();
 });
 
-tape( 'stage api_key imported from pelias config', function ( test ){
+tape( 'stage api_key imported from pelias config, old location', function ( test ){
 
   var config = '{ "mapzen": { "api_key": { "pelias.stage.mapzen.com": "my_api_key" } } }';
 
@@ -42,6 +42,38 @@ tape( 'stage api_key imported from pelias config', function ( test ){
 
   // delete temp file
   fs.unlinkSync( '/tmp/pelias_temp2.json' );
+
+  test.end();
+});
+
+tape( 'stage api_key imported from preferred config location first', function ( test ){
+  const custom_config = {
+    'acceptance-tests': {
+      credentials: {
+        'pelias.stage.mapzen.com': 'my_new_api_key'
+      }
+    },
+    mapzen: {
+      api_key: {
+        'pelias.stage.mapzen.com': 'my_old_api_key'
+      }
+    }
+  };
+
+  // write a temporary pelias config
+  fs.writeFileSync( '/tmp/pelias_temp3.json', JSON.stringify(custom_config), 'utf8' );
+
+  // set the PELIAS_CONFIG env var
+  process.env.PELIAS_CONFIG = '/tmp/pelias_temp3.json';
+
+  // staging
+  test.equal( apiKey( 'http://pelias.stage.mapzen.com/foo' ), 'my_new_api_key', 'api key loaded' );
+
+  // unset the PELIAS_CONFIG env var
+  delete process.env.PELIAS_CONFIG;
+
+  // delete temp file
+  fs.unlinkSync( '/tmp/pelias_temp3.json' );
 
   test.end();
 });
